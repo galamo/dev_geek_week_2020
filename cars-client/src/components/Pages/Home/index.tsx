e23e48a17b88e570483a279b9d88051ad8eee2d8
galamouyal <galamouyal88@gmail.com>
2020-07-13 16:25:21 +0300

    react

--- cars-client/src/components/Pages/Home/index.tsx
+++ cars-client/src/components/Pages/Home/index.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import List from "../../ui-components/list";
+import axios from "axios";
 const testArr = ["list1", 1];
+const CARS_URL = "http://localhost:4500/cars?manufacturer=audi"
+
 export default function Home() {
+    const [cars, setCars] = useState([])
+
+    useEffect(() => {
+        getCars();
+    }, [])
+
+    async function getCars() {
+        const result = await carsService();
+        const { data } = result;
+        if (!Array.isArray(data)) return;
+        //@ts-ignore
+        setCars(data);
+    }
 
     const header = "Home Page";
-
     return <div className="container">
         <h1 className="jumbotron"> {header} </h1>
-        <List data={testArr} />
+        <List data={cars} />
     </div>
 
+}
+
+async function carsService() {
+    return axios.get(CARS_URL)
 }
--- cars-client/src/components/ui-components/list/index.tsx
+++ cars-client/src/components/ui-components/list/index.tsx
@@ -1,19 +1,26 @@
 import React from "react"
 
 interface IProps {
-    data: Array<string | number>
+    data: Array<any>
+}
+
+interface Car {
+    model: string,
+    price: number,
+    manufacturer: string
 }
 
 export default function List(props: IProps) {
     const { data } = props;
     return <ul className={"list-group"}>
-        {data.map(str => getListItem(str))}
+        {data.map(car => getCarListItem(car))}
     </ul>
 }
 
 
-function getListItem(text: string | number) {
-    return <li className="list-group-item"> {text} </li>
+function getCarListItem(car: Partial<Car>) {
+    const btn = <button className="btn btn-primary" onClick={() => { console.log(car) }} style={{ float: "right" }}>Buy</button>;
+    return <li key={`${car.manufacturer}-${car.model}`} className="list-group-item"><span>Car Details:: {car.manufacturer} </span> <span> {car.model} </span> <span>  {car.price} </span> {btn}</li>
 }
 
 
